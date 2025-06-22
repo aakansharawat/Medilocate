02c19350f23c352985ccd1a96c5cd57411508f9d
aakansharawat <168464289+aakansharawat@users.noreply.github.com>
2025-06-22 12:33:59 +0530

    Add files via upload

--- backend/config.py
+++ backend/config.py
@@ -1,1 +1,6 @@
+import os
 
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://postgres:Kanurawat@localhost:5432/medilocate")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv("SECRET_KEY", "your-secret-key")
